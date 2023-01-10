429700fb1900d7a8a2f6305a5118f313cb15d5b5
benbasty <benbasty@yahoo.fr>
2023-01-10 10:25:28 +0800

    Header and hero components breaking points

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,15 @@
 import './header.css';
+import { useState } from 'react';
 import {AiFillShopping} from 'react-icons/ai';
 import LOGO from '../../assets/logo.png';
+import {GoThreeBars} from 'react-icons/go';
 function Header() {
+
+    const [ShowMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => {
+        setShowMenu(!ShowMenu)
+    }
+
     return(
         <section>
             <div className='container-header'>
@@ -10,15 +18,19 @@ function Header() {
                     <span>Ordinary</span>
                 </div>
                 <div className='nav'>
-                    <div className='menu'>
-                        <ul className='menu'>
-                            <li>Collections</li>
-                            <li>Brands</li>
-                            <li>New</li>
-                            <li>Sales</li>
-                            <li>ENG</li>
-                        </ul>
+                    
+                    <div className="bars" onClick={toggleMenu}>
+                        <GoThreeBars />
                     </div>
+                    
+                    <ul className='menu' style={{display: ShowMenu ? 'inherit':'none'}}>
+                        <li>Collections</li>
+                        <li>Brands</li>
+                        <li>New</li>
+                        <li>Sales</li>
+                        <li>ENG</li>
+                    </ul>
+                    
                     <input type="text" className='search' placeholder='Search'/>
                     <AiFillShopping className='cart'/>
                 </div>
