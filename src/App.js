77561cf099181545e70d1a65dbc61640d3607059
benbasty <benbasty@yahoo.fr>
2023-03-07 13:36:01 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       <Slider />
       <Virtual />
       <Products />
-      <Testimonials />  
-      <Footer />  
+      <Testimonials />
+      <Footer />
     </div>
   );
 }
